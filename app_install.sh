a6355d50d0a45fa9ae320eb0ac1949c41aead7fa
brotherzhafif <bangz1504@gmail.com>
2024-10-22 16:36:31 +0700

    gatau

--- app_install.sh
+++ app_install.sh
@@ -1,17 +1,18 @@
-# Add Repo
-sudo add-apt-repository “deb [arch=amd64] https://packages.microsoft.com/repos/vscode st
-
 # Updating System
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install snapd
+sudo snap install curl
 
 # Install Yang Perlu
 echo "Installing Bunch of Shit"
 sudo apt install build-essential cmake -y
 sudo apt install composer -y
 sudo apt install git
-sudo apt install npm -y
+
+v=8   # set to 4, 5, 6, ... as needed
+curl -sL https://deb.nodesource.com/setup_$v.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 sudo snap install code --classic
 # sudo snap install postman
@@ -27,5 +28,9 @@ wget https://downloadsapachefriends.global.ssl.fastly.net/xampp/7.4.30/xampp-lin
 chmod +x xampp-linux-x64-7.4.30-0-installer.run
 sudo ./xampp-linux-x64-7.4.30-0-installer.run
 echo "How to run XAMPP : sudo /opt/lampp/lampp start"
+echo "Run PHP Server With php -S localhost:8000"
 
-
+# Krita
+echo "Creative Programs"
+sudo apt install krita
+snap install blender --classic
